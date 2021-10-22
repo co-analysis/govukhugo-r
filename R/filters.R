c484fb1d8c5517d422c5fa9b38a749cdac70fe26
mattkerlogue <matt.kerlogue@me.com>
2021-10-22 18:59:32 +0100

    Add unstrap function

---
+++ R/filters.R
@@ -0,0 +1,38 @@
+#' Remove bootstrap css from crosstalk components
+#'
+#' The crosstalk injects [Bootstrap](https://getbootstrap.com/) into rendered
+#' HTML. `unstrap()` removes the Bootsrap dependencies from the components.
+#'
+#' @param x a crosstalk component, such as [`crosstalk::filter_select()`]
+#'
+#' @return
+#'
+#' The component `x` but with any `"boostrap"` dependency removed from its
+#' `html_dependencies` attribute.
+#'
+#' @examples
+#' # The fs object will inject css into your page.
+#' if (requireNamespace("crosstalk", quietly = TRUE)) {
+#'   df <- crosstalk::SharedData$new(mtcars)
+#'
+#'   fs <- crosstalk::filter_select(
+#'       id = "myselector",
+#'       label = "select something",
+#'       sharedData = df,
+#'       group = ~cyl
+#'     )
+#'
+#'   # The fs_nobootstrap object won't inject css into your page.
+#'   fs_nobootstrap <- unstrap(fs)
+#' }
+#' @export
+unstrap <- function(x) {
+  attr(x, "html_dependencies") <-
+    Filter(
+      function(dependency) {dependency$name != "bootstrap"},
+      attr(x, "html_dependencies")
+    )
+  x
+}
+
+
