d89035cfeadfa3c0d7cf798e728f03b30b9dd77b
mattkerlogue <matt.kerlogue@me.com>
2022-02-10 13:31:46 +0000

    Only build modified Rmd files by default

--- R/build.R
+++ R/build.R
@@ -8,18 +8,42 @@
 #' @param rmd_folder path to the folder containing Rmd files
 #'
 #' @export
-build_hugo_rmd <- function(rmd_folder = "R/Rmd") {
-
-  # get the MD5 checksums log
-  if (file.exists(file.path(rmd_folder, "rmd.log"))) {
-    rmd_log <- readLines(file.path(rmd_folder, "rmd.log"))
-  } else {
-    rmd_log <- NULL
-  }
+build_hugo_rmd <- function(rmd_folder = "R/Rmd", rebuild = FALSE) {
 
   # get rmd files
   rmd_files <- dir(rmd_folder, pattern = "\\.Rmd", full.names = TRUE,
                    recursive = TRUE)
+
+  if (length(rmd_files) == 0) {
+    message("No Rmarkdown files detected")
+    return(NULL)
+  }
+
+  # get the MD5 checksums log
+  if (file.exists(file.path(rmd_folder, "rmd.log"))) {
+    rmd_log <- read_md5_log(file.path(rmd_folder, "rmd.log"))
+  } else {
+    rmd_log <- NULL
+  }
+
+  new_hashes <- tools::md5sum(rmd_files)
+
+  no_build <- TRUE
+  for (hash in new_hashes) {
+    if (!hash %in% rmd_log) {
+      no_build <- FALSE
+    }
+  }
+
+  if (rebuild == TRUE) {
+    no_build <- FALSE
+    message("Site rebuild requested, unchanged Rmd files will undergo hugo-ification")
+  }
+
+  if (no_build) {
+    message("All Rmarkdown files unchanged, skipping hugo-ification")
+    return(NULL)
+  }
 
   # set a common temp directory
   tmp_dir <- tempdir()
@@ -45,7 +69,9 @@ build_hugo_rmd <- function(rmd_folder = "R/Rmd") {
   for (rmd in rmd_files) {
 
     # if md5sum unchanged don't render
-    if (check_md5(rmd, rmd_log)) {
+    if (rebuild) {
+      render_rmd(rmd, tmp_dir = tmp_dir)
+    } else if (check_md5(rmd, rmd_log)) {
       message(rmd, " unchanged, skipping hugo-ification")
     } else {
       render_rmd(rmd, tmp_dir = tmp_dir)
@@ -54,7 +80,6 @@ build_hugo_rmd <- function(rmd_folder = "R/Rmd") {
   }
 
   # re-generate hash values and write to log
-  new_hashes <- tools::md5sum(rmd_files)
   out_hashes <- paste0(rmd_files, ": ", new_hashes)
   writeLines(out_hashes, file.path(rmd_folder, "rmd.log"))
 
@@ -69,11 +94,11 @@ build_hugo_rmd <- function(rmd_folder = "R/Rmd") {
 #' @param rmd_folder path to the folder containing Rmd files
 #'
 #' @export
-build_hugo <- function(with_rmd = TRUE, rmd_folder = "R/Rmd") {
+build_hugo <- function(with_rmd = TRUE, rmd_folder = "R/Rmd", rebuild = FALSE) {
 
   # convert rmds
   if (with_rmd) {
-    build_hugo_rmd(rmd_folder)
+    build_hugo_rmd(rmd_folder, rebuild)
   }
 
   # call hugo
@@ -81,15 +106,25 @@ build_hugo <- function(with_rmd = TRUE, rmd_folder = "R/Rmd") {
 
 }
 
+# function to read the hashlog as a vector
+read_md5_log <- function(file) {
+  x <- readLines(file)
+  y <- gsub("(^.*)(: )(.*)", "\\3", x)
+  names(y) <- gsub("(^.*)(: )(.*)", "\\1", x)
+  return(y)
+}
+
 # function to check the md5sum of an rmd file against log
 check_md5 <- function(file, hash_log) {
 
   if (is.null(hash_log)) {
     FALSE
+  } else if (!(file %in% names(hash_log))) {
+    FALSE
   } else {
-    rmd_hash <- gsub(paste0("^", file, ": "), "", hash_log[grepl(file, hash_log)])
+    log_hash <- hash_log[file]
     new_hash <- tools::md5sum(file)
-    rmd_hash == new_hash
+    log_hash == new_hash
   }
 
 }
