71e49a26bf93c1d862511a26b6926eafb7e1d8f8
mattkerlogue <matt.kerlogue@me.com>
2022-06-20 16:49:20 +0100

    Further blogdown integration

--- R/serve.R
+++ R/serve.R
@@ -1,30 +1,39 @@
 #' Serve the site
 #'
-#' This is a wrapper around [blogdown::serve_site()]. It's main
-#' purpose is to ensure that the blogdown re-knitting process doesn't
-#' run when files are saved.
+#' These are wrappers around [blogdown::serve_site()] and [blogdown::stop_server()].
+#' The main purpose is to ensure that the blogdown re-knitting process doesn't
+#' run when files are saved. `stop_server()` is a simple re-export.
 #'
 #' @param ... Arguments passed to [servr::server_config()]
 #' @param .site_dir Directory to search for site configuration file, defaults to [base::getwd()].
 #'
 #' @export
 serve_site <- function(build_rmd = TRUE, rmd_folder = "R/Rmd",
-                       rebuild = FALSE) {
+                       rebuild = FALSE, drafts = TRUE, future = TRUE) {
 
   if (build_rmd) {
     build_hugo_rmd(rmd_folder = rmd_folder, rebuild = rebuild)
   }
 
-  knit_opt <- getOption("blogdown.knit.on_save")
+  options("blogdown.knit.on_save" = FALSE)
 
-  if (is.null(knit_opt)) {
-    options("blogdown.knit.on_save" = FALSE)
-    cli::cli_warn("Global option {.var blogdown.knit.on_save} has been set to {.val FALSE}")
-  } else if (knit_opt) {
-    options("blogdown.knit.on_save" = FALSE)
-    cli::cli_warn("Global option {.var blogdown.knit.on_save} has been set to {.val FALSE}")
+  if (drafts & future) {
+    options("blogdown.hugo.server" = c("-D", "-F"))
+  } else if (drafts) {
+    options("blogdown.hugo.server" = c("-D"))
+  } else if (future) {
+    options("blogdown.hugo.server" = c("-F"))
+  } else {
+    options("blogdown.hugo.server" = "")
   }
+
 
   blogdown::serve_site()
 
+}
+
+#' @describeIn serve_site Stop serving a Hugo site
+#' @export
+stop_server <- function() {
+  blogdown::stop_server()
 }
